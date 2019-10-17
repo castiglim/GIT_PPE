1702223a54d41229e9118909fc7ab249916f6455
Maxence CASTIGLIA <maxence.castiglia@icof.fr>
2019-10-17 15:53:30 +0200

    test3

--- menu.php
+++ menu.php
@@ -6,6 +6,6 @@
 		<a class="lien" href="index.php?page=test2">|Les géants|&emsp;</a>
 		<a class="lien" href="index.php?page=catalogue">|Les paires prisées|&emsp;</a>
 		<a class="lien" href="index.php?page=test3">|Contact|</a>
-		<!--enorme test -->
+		<!--adrien le plus beau -->
 	</ul>
 </nav>	
