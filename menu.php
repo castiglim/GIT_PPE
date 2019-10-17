67ac10962317035c6aa75d9c3c37f316f14e2969
Maxence CASTIGLIA <maxence.castiglia@icof.fr>
2019-10-17 15:00:05 +0200

    test2

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 		Aller :&emsp;&emsp;
 		<STYLE>A {text-decoration: none;} </STYLE>
 		<a class="lien" href="index.php?page=test1">|Les origines|&emsp;</a>		  
-		<a class="lien" href="index.php?page=test2">|Les trop forts|&emsp;</a>
+		<a class="lien" href="index.php?page=test2">|Les t545845454rop forts|&emsp;</a>
 		<a class="lien" href="index.php?page=catalogue">|Les paires prisées|&emsp;</a>
 		<a class="lien" href="index.php?page=test3">|Contact|</a>
 	</ul>
