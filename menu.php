18ca1b40387307790b2536f367b48e0048681228
Maxence CASTIGLIA <maxence.castiglia@icof.fr>
2019-10-17 15:03:30 +0200

    changement

--- menu.php
+++ menu.php
@@ -3,8 +3,9 @@
 		Aller :&emsp;&emsp;
 		<STYLE>A {text-decoration: none;} </STYLE>
 		<a class="lien" href="index.php?page=test1">|Les origines|&emsp;</a>		  
-		<a class="lien" href="index.php?page=test2">|Les t545845454rop forts|&emsp;</a>
+		<a class="lien" href="index.php?page=test2">|Les géants|&emsp;</a>
 		<a class="lien" href="index.php?page=catalogue">|Les paires prisées|&emsp;</a>
 		<a class="lien" href="index.php?page=test3">|Contact|</a>
+		<!--enorme test -->
 	</ul>
 </nav>	
